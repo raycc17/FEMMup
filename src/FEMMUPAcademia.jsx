a6982fe3aa8aeb5e775ddacb0abbd58bcfa985eb
raycc17 <raycoroneldelcarpio@gmail.com>
2026-02-12 18:07:32 -0500

    FEMMUPAcademia.jsx

--- src/FEMMUPAcademia.jsx
+++ src/FEMMUPAcademia.jsx
@@ -21,7 +21,7 @@ export default function FEMMUPAcademia() {
           <div className="text-4xl md:text-5xl font-serif font-extrabold leading-none tracking-tight">
             <span className="bg-gradient-to-r from-[#c79a9a] via-[#7a5a5a] to-[#000000] bg-clip-text text-transparent">
 <img
-  src="/logo-femmup.png"
+  src="logo-femmup.png."
   alt="FEMMUP logo"
   className="h-12 md:h-14 w-auto object-contain"
 />
